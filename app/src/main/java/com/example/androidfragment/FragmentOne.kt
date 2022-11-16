d0191d9929a7805618ca92655ecf2508cf561714
DanielAndroid <humbleson32@gmail.com>
2022-11-15 21:21:36 -0400

    Initial Commit

--- app/src/main/java/com/example/androidfragment/FragmentOne.kt
+++ app/src/main/java/com/example/androidfragment/FragmentOne.kt
@@ -6,27 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [FragmentOne.newInstance] factory method to
  * create an instance of this fragment.
  */
 class FragmentOne : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -40,13 +28,9 @@ class FragmentOne : Fragment() {
     companion object {
         /**
          * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * the fragment.
          * @return A new instance of fragment MyFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() = FragmentOne()
 
